3cdc989709655adeb65cfe3ca1827dd9e8e6ef7a
Flier Lu <flier.lu@gmail.com>
2015-04-13 01:38:37 +0800

    add CURD example

---
+++ examples/framework/crud.go
@@ -0,0 +1,15 @@
+package framework
+
+import (
+	"github.com/flier/curator.go"
+)
+
+func Create(client curator.CuratorFramework, path string, payload []byte) error {
+	// this will create the given ZNode with the given data
+	return client.Create().ForPathWithData(path, payload)
+}
+
+func CreateEphemeral(client curator.CuratorFramework, path string, payload []byte) error {
+	// this will create the given EPHEMERAL ZNode with the given data
+	return client.Create().WithMode(curator.EPHEMERAL).ForPathWithData(path, payload)
+}
