f9f4264d2b7c9de43646bb227709f9155a3ef828
Flier Lu <flier.lu@gmail.com>
2015-04-13 19:05:28 +0800

    create retryLoop base on retryPolicy

--- client.go
+++ client.go
@@ -1,15 +1,25 @@
 package curator
 
 import (
+	"time"
+
 	"github.com/samuel/go-zookeeper/zk"
 )
 
 type CuratorZookeeperClient struct {
-	state ZookeeperConnectionState
+	state             *ZookeeperConnectionState
+	retryPolicy       RetryPolicy
+	connectionTimeout time.Duration
+	tracer            TracerDriver
 }
 
-func NewCuratorZookeeperClient() *CuratorZookeeperClient {
-	return &CuratorZookeeperClient{}
+func NewCuratorZookeeperClient(ensembleProvider EnsembleProvider, sessionTimeout, connectionTimeout time.Duration, retryPolicy RetryPolicy, canReadOnly bool) *CuratorZookeeperClient {
+	return &CuratorZookeeperClient{
+		state:             newZookeeperConnectionState(ensembleProvider, sessionTimeout, connectionTimeout, retryPolicy, canReadOnly),
+		retryPolicy:       retryPolicy,
+		connectionTimeout: connectionTimeout,
+		tracer:            newDefaultTracerDriver(),
+	}
 }
 
 func (c *CuratorZookeeperClient) Start() error {
@@ -20,6 +30,18 @@ func (c *CuratorZookeeperClient) Close() error {
 	return nil
 }
 
+func (c *CuratorZookeeperClient) newRetryLoop() *retryLoop {
+	return newRetryLoop(c.retryPolicy, c.tracer)
+}
+
+func (c *CuratorZookeeperClient) Zookeeper() *zk.Conn {
+	return c.state.conn
+}
+
 type ZookeeperConnectionState struct {
 	conn *zk.Conn
+}
+
+func newZookeeperConnectionState(ensembleProvider EnsembleProvider, sessionTimeout, connectionTimeout time.Duration, retryPolicy RetryPolicy, canReadOnly bool) *ZookeeperConnectionState {
+	return &ZookeeperConnectionState{}
 }
