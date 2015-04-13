535e29ba06cb9426400868397e50ea59b3be72d5
Flier Lu <flier.lu@gmail.com>
2015-04-13 19:04:51 +0800

    add ACLProvider interface methods

--- acl.go
+++ acl.go
@@ -1,4 +1,13 @@
 package curator
 
+import (
+	"github.com/samuel/go-zookeeper/zk"
+)
+
 type ACLProvider interface {
+	// Return the ACL list to use by default
+	GetDefaultAcl() []zk.ACL
+
+	// Return the ACL list to use for the given path
+	GetAclForPath(path string) []zk.ACL
 }
