8d050be65469b8f41bbaf1ca3afc6a01d9f4e8a9
Flier Lu <flier.lu@gmail.com>
2015-04-14 19:05:57 +0800

    close Closable quietly with recover and log

---
+++ utils.go
@@ -0,0 +1,27 @@
+package curator
+
+import (
+	"github.com/golang/glog"
+)
+
+// A Closeable is a source or destination of data that can be closed.
+type Closeable interface {
+	// Closes this and releases any system resources associated with it.
+	Close() error
+}
+
+func CloseQuietly(closeable Closeable) (err error) {
+	defer func() {
+		if v := recover(); v != nil {
+			glog.Fatalf("panic when closing %s, %v", closeable, v)
+
+			err, _ = v.(error)
+		}
+	}()
+
+	if err = closeable.Close(); err != nil {
+		glog.Errorf("fail to close %s, %s", closeable, err)
+	}
+
+	return
+}
