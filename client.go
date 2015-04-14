22b2d450b32617498274ec8788fea452e25a33b6
Flier Lu <flier.lu@gmail.com>
2015-04-14 19:11:55 +0800

    add ZookeeperDialer as factory

--- client.go
+++ client.go
@@ -1,51 +1,123 @@
 package curator
 
 import (
+	"errors"
+	"strings"
 	"time"
 
+	"github.com/golang/glog"
 	"github.com/samuel/go-zookeeper/zk"
 )
 
-type CuratorZookeeperClient struct {
-	state             *ZookeeperConnectionState
-	retryPolicy       RetryPolicy
-	connectionTimeout time.Duration
-	tracer            TracerDriver
+// Allocate a new ZooKeeper connection
+type ZookeeperDialer interface {
+	Dial(connString string, sessionTimeout time.Duration, canBeReadOnly bool) (*zk.Conn, <-chan zk.Event, error)
 }
 
-func NewCuratorZookeeperClient(ensembleProvider EnsembleProvider, sessionTimeout, connectionTimeout time.Duration, retryPolicy RetryPolicy, canReadOnly bool) *CuratorZookeeperClient {
+type DefaultZookeeperDialer struct {
+	Dialer zk.Dialer
+}
+
+func (d *DefaultZookeeperDialer) Dial(connString string, sessionTimeout time.Duration, canBeReadOnly bool) (*zk.Conn, <-chan zk.Event, error) {
+	return zk.ConnectWithDialer(strings.Split(connString, ","), sessionTimeout, d.Dialer)
+}
+
+type CuratorZookeeperClient struct {
+	state             *ZookeeperConnectionState
+	connectionTimeout time.Duration
+	watcher           Watcher
+	started           bool
+	TracerDriver      TracerDriver
+	RetryPolicy       RetryPolicy
+}
+
+func NewCuratorZookeeperClient(zookeeperDialer ZookeeperDialer, ensembleProvider EnsembleProvider, sessionTimeout, connectionTimeout time.Duration,
+	watcher Watcher, retryPolicy RetryPolicy, canReadOnly bool) *CuratorZookeeperClient {
+
+	if sessionTimeout < connectionTimeout {
+		glog.Warningf("session timeout [%d] is less than connection timeout [%d]", sessionTimeout, connectionTimeout)
+	}
+
+	tracer := newDefaultTracerDriver()
+
 	return &CuratorZookeeperClient{
-		state:             newZookeeperConnectionState(ensembleProvider, sessionTimeout, connectionTimeout, retryPolicy, canReadOnly),
-		retryPolicy:       retryPolicy,
+		state:             newZookeeperConnectionState(zookeeperDialer, ensembleProvider, sessionTimeout, connectionTimeout, watcher, tracer, canReadOnly),
 		connectionTimeout: connectionTimeout,
-		tracer:            newDefaultTracerDriver(),
+		TracerDriver:      tracer,
+		RetryPolicy:       retryPolicy,
 	}
 }
 
 func (c *CuratorZookeeperClient) Start() error {
-	return nil
+	if c.started {
+		return errors.New("Already started")
+	}
+
+	return c.state.Start()
 }
 
 func (c *CuratorZookeeperClient) Close() error {
-	return nil
+	c.started = false
+
+	return c.state.Close()
+}
+
+// Returns true if the client is current connected
+func (c *CuratorZookeeperClient) IsConnected() bool {
+	return c.state.isConnected()
+}
+
+func (c *CuratorZookeeperClient) CurrentConnectionString() string {
+	return c.state.ensembleProvider.ConnectionString()
 }
 
 func (c *CuratorZookeeperClient) newRetryLoop() *retryLoop {
-	return newRetryLoop(c.retryPolicy, c.tracer)
+	return newRetryLoop(c.RetryPolicy, c.TracerDriver)
 }
 
 func (c *CuratorZookeeperClient) startTracer(name string) Tracer {
-	return newTimeTrace(name, c.tracer)
+	return newTimeTrace(name, c.TracerDriver)
 }
 
-func (c *CuratorZookeeperClient) Conn() *zk.Conn {
-	return c.state.conn
+func (c *CuratorZookeeperClient) Conn() (*zk.Conn, error) {
+	return c.state.Conn()
 }
 
-type ZookeeperConnectionState struct {
-	conn *zk.Conn
+// This method blocks until the connection to ZK succeeds.
+func (c *CuratorZookeeperClient) BlockUntilConnectedOrTimedOut() error {
+	if !c.started {
+		return errors.New("Client is not started")
+	}
+
+	tracer := c.startTracer("blockUntilConnectedOrTimedOut")
+
+	defer tracer.Commit()
+
+	c.internalBlockUntilConnectedOrTimedOut()
+
+	if c.state.isConnected() {
+		return nil
+	}
+
+	return errors.New("Client connect timeouted")
 }
 
-func newZookeeperConnectionState(ensembleProvider EnsembleProvider, sessionTimeout, connectionTimeout time.Duration, retryPolicy RetryPolicy, canReadOnly bool) *ZookeeperConnectionState {
-	return &ZookeeperConnectionState{}
+func (c *CuratorZookeeperClient) internalBlockUntilConnectedOrTimedOut() error {
+	timer := time.NewTimer(c.connectionTimeout)
+	connected := make(chan error)
+
+	watcher := c.state.addParentWatcher(func(*zk.Event) {
+		if c.state.isConnected() {
+			connected <- nil
+		}
+	})
+
+	defer c.state.removeParentWatcher(watcher)
+
+	select {
+	case err := <-connected:
+		return err
+	case <-timer.C:
+		return errors.New("Client connect timeouted")
+	}
 }
